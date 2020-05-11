3146254f832a0d2167b647109e5096380d83d496
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-11 23:36:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,6 +9,8 @@ end
 
 create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
-# project.css("h2.bbcard_name strong a").text
-# project.css("div.project-thumbnail a img").attribute("src").value
-# project.css("p.bbcard_blurb").text
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta li a span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text
