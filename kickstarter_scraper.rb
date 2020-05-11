7d644b117d2ce405642347fb86dbaff29bcd46bd
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-11 22:46:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'pry'
 
-def create_project_hash
+def create_project_hash # projects: kickstarter.css("li.project.grid_4")
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
